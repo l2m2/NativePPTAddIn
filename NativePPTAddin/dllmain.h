0d41a94b1abf9d49066d5a2c3c97729e240ddc89
l2m2 <l2m2lq@gmail.com>
2021-10-22 14:51:44 +0800

    添加项目文件。

---
+++ NativePPTAddin/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: 模块类的声明。
+
+class CNativePPTAddinModule : public ATL::CAtlDllModuleT< CNativePPTAddinModule >
+{
+public :
+	DECLARE_LIBID(LIBID_NativePPTAddinLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_NATIVEPPTADDIN, "{88a0e8ac-0fbd-4973-a824-210f272d5240}")
+};
+
+extern class CNativePPTAddinModule _AtlModule;
